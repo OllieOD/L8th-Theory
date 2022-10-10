e11f52ecffcca47f294e8d1092bc86053dd651cd
OllieOD <odonoho@stpaulsschool.org.uk>
2022-10-10 14:37:09 +0100

    a

--- Program.cs
+++ Program.cs
@@ -21,27 +21,37 @@ namespace L8th_Theory
             string final = "";
             for (int i = 0; i < compress.Length; i++)
             {
-                    for (int x = 0; x < compress.Length; x++)
+                //    for (int x = 0; x < compress.Length; x++)
+                //{
+                //    if (compress[x] == compress[i] )
+                //    {
+                //        count++;
+                //    }
+                //    else if (compress[x] != compress[i])
+                //    {
+                //        final = final + compress[i] + count;
+
+
+                //    }
+
+
+
+                if (i == 0 || compress[i] == compress[i - 1])
                 {
-                    if (compress[x] == compress[i])
-                    {
-                        count++;
-                    }
-                    else if (compress[x] != compress[i])
+                    count++;
+                    if (i == (compress.Length - 1) || compress[i] != compress[i + 1])
                     {
                         final = final + compress[i] + count;
-                        
-                        
+                        count = 0;
                     }
-                  
-                    
+
                 }
-                   
-
-
-
-
             }
+
+
+
+
+            
             Console.WriteLine(final);
         }
     }
